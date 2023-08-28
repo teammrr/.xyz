9164beb33149d2f8512c3af06d88645e8cf55f58
Suteemon <suteemon.team@newton.ac.th>
2023-08-29 00:51:33 +0700

    Few Changes in timeline

--- src/routes/timeline/+page.svelte
+++ src/routes/timeline/+page.svelte
@@ -102,15 +102,7 @@
 					<h3 class="text-lg font-semibold text-gray-900 dark:text-white">Exploring More Frameworks & Bootstrap</h3>
 					<p class="text-base font-normal text-gray-500 dark:text-gray-400">I am currently trying out different types of technology and will make a project out of it. Stay tuned!!</p>
 				</li>
-				<li class="ml-4">
-					<div class="absolute w-3 h-3 bg-gray-200 rounded-full mt-1.5 -left-1.5 border border-white dark:border-gray-900 dark:bg-gray-700"></div>
-					<time class="mb-1 text-sm font-normal leading-none text-gray-400 dark:text-gray-500">November 2023</time>
-					<h3 class="text-lg font-semibold text-gray-900 dark:text-white">Personal Project</h3>
-					<p class="text-base font-normal text-gray-500 dark:text-gray-400">More info releasing soon.</p>
-				</li>
 			</ol>
-			
-
 		</div>
 		
 	</div>
