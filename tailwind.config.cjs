f9edc504b48dca6770f09604d5c43f62a58306f3
Suteemon <suteemon.team@newton.ac.th>
2023-09-03 21:02:30 +0700

    Hiding Menu Bar on Mobile

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -5,8 +5,5 @@ import skeleton from '@skeletonlabs/skeleton/tailwind/skeleton.cjs'
 module.exports = {
 	darkMode: 'class',
 	content: ['./src/**/*.{html,js,svelte,ts}', join(require.resolve('@skeletonlabs/skeleton'), '../**/*.{html,js,svelte,ts}')],
-	theme: {
-		extend: {},
-	},
-	plugins: [...skeleton()],
+	plugins: [...skeleton()],		
 }
